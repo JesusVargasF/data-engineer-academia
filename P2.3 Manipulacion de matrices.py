bdc9fb8841c6f074af0f9c8f5c12485294656587
Jose de Jesus Vargas Figueroa <vargasfigueroajosedejesus@gmail.com>
2025-01-16 19:22:30 -0600

    Delete P2.3 Manipulacion de matrices.py

--- P2.3 Manipulacion de matrices.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-matriz = np. array ([[1, 2], [3, 4]]) 
-
-transpuesta = matriz.T  # [[1, 3], [2, 4]] 
-
-print("Matriz original: ", matriz, "\nMatriz transpuesta: ", transpuesta)
