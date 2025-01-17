a7afb8f2075394749ca0afaad1ca89ecb80ea3f4
Jose de Jesus Vargas Figueroa <vargasfigueroajosedejesus@gmail.com>
2025-01-16 19:22:11 -0600

    Delete P4.2 Ejercicio Ventas.py

--- P4.2 Ejercicio Ventas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel('ventas-x-ciudad.xlsx') 
-
-print("Dataframe original:\n",df)
-
-ventas_por_ciudad = df.groupby('Ciudad')['Ventas'].sum() 
-
-print("Ventas por ciudad:\n", ventas_por_ciudad)
